373239f954f6a43ca0b7a4009979b172120d637a
王超 <chao.wang@hqq.vip>
2017-11-09 18:44:26 +0800

    db adapter profiler

--- src/Adapter/Profiler/Service/ProfilerFactory.php
+++ src/Adapter/Profiler/Service/ProfilerFactory.php
@@ -11,8 +11,8 @@ class ProfilerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $name, array $options = null)
     {
-    	$profile = new Profiler();
-    	$profile->setLogger($container->get('Kernel\Log\Logger'));
+        $profile = new Profiler();
+        $profile->setLogger($container->get('Kernel\Log\Logger'));
 
         return $profile;
     }
